360b2c0a8cbdfc59eab2d69947f2d2c50233cd04
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-26 17:41:43 -0500

    Add regquirements.txt file.

--- blank_row_inserter.py
+++ blank_row_inserter.py
@@ -1,8 +1,17 @@
-#! python3
+#!/usr/bin/env python3
 # blankRowInserter.py — An exercise in manipulating Excel files.
+# For more information, see README.md
 
+import logging
 import sys
 import openpyxl
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="logging.txt",
+    format="%(asctime)s -  %(levelname)s -  %(message)s",
+)
+logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 row_location = int(sys.argv[1])
 number_of_rows = int(sys.argv[2])
